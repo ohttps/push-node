b6f2bf32191e40766bfecc7893b5bd0bab32ca98
ohttps <service@ohttps.com>
2023-10-22 16:19:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,115 @@
+# OHTTPS
+
+OHTTPS 致力于帮助用户申请免费 HTTPS/SSL 证书，提供证书**自动化更新、自动化部署、自动化监控**服务
+
+目前已支持自动化部署证书至：[阿里云](https://ohttps.com/docs/cloud/aliyun/)、[腾讯云](https://ohttps.com/docs/cloud/tcloud/)、[Docker容器](https://ohttps.com/docs/cloud/docker/)、[七牛云](https://ohttps.com/docs/cloud/qiniu/)、[宝塔面板](https://ohttps.com/docs/cloud/btcn/)、[百度云加速](https://ohttps.com/docs/cloud/bdyjs/)、[Webhook](https://ohttps.com/docs/cloud/webhook/)、[SSH](https://ohttps.com/docs/cloud/ssh/)、[API接口](https://ohttps.com/docs/cloud/api/) 等
+
+官网：[https://ohttps.com](https://ohttps.com)
+
+文档：[https://ohttps.com/docs/start](https://ohttps.com/docs/start)
+
+
+# push-node
+
+push-node 是 OHTTPS 中为实现证书实时自动化部署至 **自定义容器** 而开发的证书更新服务
+
+其原理是内部通过 socket.io 订阅 OHTTPS 服务器端证书更新事件，实现自定义容器内证书的实时自动更新
+
+nginx、httpd、openresty 等服务的 docker 容器可结合 push-node，实现相应服务的证书自动化更新
+
+除了与容器结合使用外，亦可独立使用，用户可根据自身的需求自行判断如何使用更为便捷
+
+
+#### 如何使用
+1. 在 OHTTPS 中创建 **[容器 - 自定义]** 类型的部署节点
+2. 在 OHTTPS 中将需要部署至该节点的证书与该节点进行绑定
+3. 编写容器 Dockerfile 文件，可参考下方[使用示例01](#使用示例01)
+4. 通过 Dockerfile 文件构建容器镜像，并创建容器实例
+5. 容器实例启动后 push-node 便会自动将已绑定的证书文件从服务器端拉取到本地
+6. push-node 在证书首次拉取成功后，会执行 service-start-cmd，用以启动使用证书的相关服务，例如 nginx 等
+7. push-node 在证书每次更新成功后，会执行 service-reload-cmd，用以重启使用证书的相关服务，例如 nginx 等
+
+#### 参数说明
+* node-id：
+    * 必填，在 OHTTPS 中申请的 **[容器 - 自定义]** 类型部署节点 ID
+* node-token：
+    * 必填，在 OHTTPS 中申请的 **[容器 - 自定义]** 类型部署节点 TOKEN
+* cert-folder：
+    * 非必填，证书所在文件夹，默认为 “/etc/ohttps/certificates/”
+    * 例如部署节点已绑定证书“cert-2enm4pr1q09g3x5z”，那么该证书相应证书文件的路径为：
+        * 私钥文件cert.key（PEM格式）：/etc/ohttps/certificates/cert-2enm4pr1q09g3x5z/cert.key
+        * 证书文件fullchain.cer（PEM格式）：/etc/ohttps/certificates/cert-2enm4pr1q09g3x5z/fullchain.cer
+* service-start-cmd：
+    * 非必填，证书文件首次下载后执行的启动命令，例如“nginx -c /etc/nginx/nginx.conf”，可实现在证书首次下载完成后启动nginx
+* service-reload-cmd：
+    * 非必填，证书文件每次更新后执行的重载命令，例如“nginx -c /etc/nginx/nginx.conf -s reload”，实现在证书每次更新后重启nginx
+
+
+#### 二进制包
+* push-node-alpine-arm64：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-alpine-arm64
+* push-node-alpine-x64：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-alpine-x64
+* push-node-linux-arm64：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-linux-arm64
+* push-node-linux-x64：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-linux-x64
+* push-node-macos-arm64：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-macos-arm64
+* push-node-macos-x64：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-macos-x64
+* push-node-win-arm64.exe：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-win-arm64.exe
+* push-node-win-x64.exe：
+    * https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-win-x64.exe
+* 注意：**使用时请根据系统架构自行选择合适的二进制包**
+* 注意：**最新版本请查看[release](https://github.com/ohttps/push-node/releases)列表**
+
+
+#### 使用示例01
+**配合容器使用，系统为 alpine-x64，服务为 nginx**
+
+nginx 容器结合 push-node，实现 nginx 容器内的证书自动化更新
+
+可查看本仓库中的完整示例代码：https://github.com/ohttps/push-node/tree/main/examples/nginx
+
+注意：**需提前下载push-node-alpine-x64二进制文件到本地，通过COPY命令复制进容器镜像中**
+
+```Dockerfile
+FROM nginx:stable-alpine
+# 删除官方容器镜像中的默认日志
+# 否则启动nginx命令会报日志权限异常
+RUN unlink /var/log/nginx/access.log
+RUN unlink /var/log/nginx/error.log
+# 设置当前工作文件夹
+WORKDIR /etc/nginx
+# 复制nginx配置文件
+COPY ./nginx.conf .
+# 复制push-node二进制包
+COPY ./push-node-alpine-x64 .
+# 为push-node添加可执行权限
+RUN chmod a+x ./push-node-alpine-x64
+# 设置容器启动命令
+ENTRYPOINT ["./push-node-alpine-x64", "--node-id=push-mlyxpro7vo0ez6jw", "--node-token=b4f2f70c85466671bc06d7f1f469395d", "--cert-folder=/etc/nginx/certificates/", "--service-start-cmd='nginx'", "--service-reload-cmd='nginx -s reload'"]
+```
+
+
+#### 使用示例02
+**独立使用，系统为 macos-x64，服务为 nginx**
+
+nginx 结合 push-node，实现 nginx 内证书自动化更新
+
+```shell
+// 下载macos-x64系统对应的push-node二进制文件
+wget https://github.com/ohttps/push-node/releases/download/1.0.0/push-node-macos-x64
+
+// 为push-node添加执行权限
+chmod a+x ./push-node-macos-x64
+
+// 启动push-node
+./push-node-macos-x64 --node-id=push-mlyxpro7vo0ez6jw \
+    --node-token=b4f2f70c85466671bc06d7f1f469395d \
+    --cert-folder=./certificates/ \
+    --service-start-cmd="nginx" \
+    --service-reload-cmd="nginx -s reload"
+```
---
+++ src/push-node.js
@@ -0,0 +1,208 @@
+import fs from 'fs'
+import path from 'path'
+import process from 'process'
+import { exec } from 'child_process'
+import io from 'socket.io-client'
+
+// 获取入参
+const processArgs = getProcessArgs()
+
+// OHTTPS中创建的部署节点ID
+const PUSH_NODE_ID = processArgs['node-id'] || process.env.PUSH_NODE_ID
+// OHTTPS中创建的部署节点令牌
+const PUSH_NODE_TOKEN = processArgs['node-token'] || process.env.PUSH_NODE_ID
+// OHTTPS的SOCKET服务地址
+const PUSH_SOCKET_URL = processArgs['socket-url'] || process.env.PUSH_SOCKET_URL || 'https://socket.ohttps.com/'
+// 证书文件首次下载后执行的启动命令
+const PUSH_NODE_SERVICE_START_CMD = processArgs['service-start-cmd'] || process.env.PUSH_NODE_SERVICE_START_CMD
+// 证书文件每次更新后执行的重载命令
+const PUSH_NODE_SERVICE_RELOAD_CMD = processArgs['service-reload-cmd'] || process.env.PUSH_NODE_SERVICE_RELOAD_CMD
+// 证书文件所在文件夹
+let PUSH_NODE_FOLDER = processArgs['cert-folder'] || process.env.PUSH_NODE_FOLDER || '/etc/ohttps/certificates/'
+if (!PUSH_NODE_FOLDER.endsWith('/')) {
+  PUSH_NODE_FOLDER = `${PUSH_NODE_FOLDER}/`
+}
+// 单个证书的文件路径
+const CERTIFICATE_FILE_PATH = {
+  FOLDER: PUSH_NODE_FOLDER,
+  KEY: (certificateName) => `${PUSH_NODE_FOLDER}${certificateName}/cert.key`,
+  FULLCHAIN: (certificateName) => `${PUSH_NODE_FOLDER}${certificateName}/fullchain.cer`
+}
+
+if (!PUSH_NODE_ID) {
+  debug('[部署节点ID缺失|push-node-id-missing]: PUSH_NODE_ID')
+  process.exit()
+}
+
+if (!PUSH_NODE_TOKEN) {
+  debug('[部署节点TOKEN缺失|push-node-token-missing]: PUSH_NODE_TOKEN')
+  process.exit()
+}
+
+debug('[参数配置|configuration]')
+debug('  [PUSH_NODE_ID]:', PUSH_NODE_ID)
+debug('  [PUSH_NODE_TOKEN]:', PUSH_NODE_TOKEN)
+debug('  [PUSH_SOCKET_URL]:', PUSH_SOCKET_URL || '')
+debug('  [PUSH_NODE_FOLDER]:', PUSH_NODE_FOLDER || '')
+debug('  [PUSH_NODE_SERVICE_START_CMD]:', PUSH_NODE_SERVICE_START_CMD || '')
+debug('  [PUSH_NODE_SERVICE_RELOAD_CMD]:', PUSH_NODE_SERVICE_RELOAD_CMD || '')
+
+let isFirstTimeLoad = true
+const socket = io(PUSH_SOCKET_URL, { transports: ['websocket'], transportOptions: { forceNode: true } })
+debug('')
+debug('[开始连接服务器|begin-connect-to-server]')
+
+socket.on('connect', () => {
+  if (!isFirstTimeLoad) {
+    debug('')
+  }
+  debug(`[服务器${isFirstTimeLoad ? '连接' : '重新连接'}成功|${isFirstTimeLoad ? 'connect' : 'reconnect'}-to-server-success]`)
+
+  debug(`[开始校验部署节点参数|begin-verify-push-node]`)
+  socket.emit('login', {
+    cloudServerName: PUSH_NODE_ID,
+    cloudServerToken: PUSH_NODE_TOKEN
+  }, async (response) => {
+    if (response.error) {
+      debug('[部署节点参数校验失败|verify-push-node-failed]:', response.error)
+      debug('[请检查部署节点ID或TOKEN是否配置正确|please-check-the-push-node-id-and-token]')
+      return
+    }
+    debug('[部署节点参数校验成功|verify-push-node-succeed]')
+
+    if (!isFirstTimeLoad) {
+      return
+    }
+
+    debug('')
+    debug(`↓↓↓===================首次证书部署-${getCurrentTime()}===================↓↓↓`)
+    debug('[开始首次加载部署节点关联证书|begin-load-certificates-for-the-first-time]')
+    isFirstTimeLoad = false
+    const updateResult = await updateCertificates({ certificates: response.certificates, isFirstTimeLoad: true })
+    debug('[首次加载部署节点关联证书完成|load-certificates-for-the-first-time-finished]: ', updateResult)
+    debug(`↑↑↑===================首次证书部署-${getCurrentTime()}===================↑↑↑`)
+  })
+})
+
+socket.on('disconnect', () => {
+  debug('')
+  debug('[已断开与服务器连接|server-disconnected]')
+})
+
+socket.on('updateCertificate', async (data, cb) => {
+  debug('')
+  debug(`↓↓↓===================证书更新部署-${getCurrentTime()}===================↓↓↓`)
+  debug('[开始处理部署节点关联证书更新事件|receive-certificates-update-event]:', data.certificateName)
+  const updateResult = await updateCertificates({ certificates: [data], isFirstTimeLoad: false })
+  debug('[部署节点关联证书更新事件处理完成|certificates-update-event-finished]:', JSON.stringify(updateResult))
+  debug(`↑↑↑===================证书更新部署-${getCurrentTime()}===================↑↑↑`)
+  cb(updateResult)
+})
+
+async function updateCertificates({ certificates, isFirstTimeLoad }) {
+  debug('[是否是首次证书部署|is-first-time-load]:', isFirstTimeLoad)
+  debug('[需要部署的证书数量|certificates-num]:', certificates.length)
+  debug('[证书部署目标文件夹|certificates-folder]:', path.resolve(CERTIFICATE_FILE_PATH.FOLDER))
+
+  // 创建证书文件夹
+  if (!fs.existsSync(CERTIFICATE_FILE_PATH.FOLDER)) {
+    fs.mkdirSync(CERTIFICATE_FILE_PATH.FOLDER, { recursive: true })
+    debug('[证书部署目标文件夹创建成功|certificates-folder-created]')
+  }
+
+  for (let i = 0; i < certificates.length; i++) {
+    const certificate = certificates[i]
+    const certificateFoler = path.resolve(`${CERTIFICATE_FILE_PATH.FOLDER}${certificate.certificateName}`)
+    const certificateCertKeyPath = path.resolve(CERTIFICATE_FILE_PATH.KEY(certificate.certificateName))
+    const certificateFullchainCertsPath = path.resolve(CERTIFICATE_FILE_PATH.FULLCHAIN(certificate.certificateName))
+
+    debug(`[开始部署第N个证书|begin-deploy-nth-certificate]: ${i + 1}/${certificates.length}, ${certificate.certificateName}`)
+
+    debug('  [证书文件夹|certificate-folder]:', certificateFoler)
+    // 创建单个证书文件夹
+    if (!fs.existsSync(certificateFoler)) {
+      fs.mkdirSync(certificateFoler, { recursive: true })
+      debug('  [证书文件夹创建成功|certificate-folder-created]')
+    }
+
+    debug('  [证书私钥文件路径|private-key-file-path]:', certificateCertKeyPath)
+    fs.writeFileSync(certificateCertKeyPath, certificate.certificateCertKey)
+    debug('  [证书私钥文件写入成功|private-key-file-created]')
+
+    debug('  [证书公钥文件路径|public-key-file-path]:', certificateFullchainCertsPath)
+    fs.writeFileSync(certificateFullchainCertsPath, certificate.certificateFullchainCerts)
+    debug('  [证书公钥文件写入成功|public-key-file-created]')
+  }
+
+  const cmd = isFirstTimeLoad ? PUSH_NODE_SERVICE_START_CMD : PUSH_NODE_SERVICE_RELOAD_CMD
+  const cmdCnName = isFirstTimeLoad ? '启动' : '重启'
+  const cmdEnName = isFirstTimeLoad ? 'start' : 'reload'
+  if (!cmd) {
+    if (isFirstTimeLoad) {
+      debug(`[未配置服务${cmdCnName}命令|service-${cmdEnName}-cmd-not-config]`)
+    } else {
+      debug(`[未配置服务${cmdCnName}命令|service-${cmdEnName}-cmd-not-config]`)
+    }
+    debug('[命令执行跳过|skip-cmd-excute]')
+    return { cmd: '', code: 0, stdout: 'sucess', stderr: '' }
+  }
+
+  debug(`[开始执行服务${cmdCnName}命令|begin-excute-service-${cmdEnName}-cmd]:`, cmd)
+  const { code, stdout, stderr } = await (new Promise((resolve, _) => {
+    exec(cmd, (error, stdout, stderr) => {
+      resolve({ code: error ? error.code : 0, stdout, stderr })
+    })
+  }))
+
+  if (stderr) {
+    debug(`[命令执行异常|cmd-excute-error]:`, stderr)
+  }
+
+  debug(`[服务${cmdCnName}命令执行完成|excute-service-${cmdEnName}-cmd-complete]:`, cmd)
+  return { cmd, code, stdout, stderr }
+}
+
+// 日志
+function debug() {
+  console.log(getCurrentTime(), '•', 'OHTTPS', '•', ...arguments)
+}
+
+// 获取当前时间字符串
+function getCurrentTime() {
+  const now = new Date();
+  return `${now.getFullYear()}-${(now.getMonth() + 1)
+    .toString()
+    .padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now
+      .getHours()
+      .toString()
+      .padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now
+        .getSeconds()
+        .toString()
+        .padStart(2, '0')}.${now
+          .getMilliseconds()
+          .toString()
+          .padStart(3, '0')}`
+}
+
+// 入参
+function getProcessArgs() {
+  const args = {}
+  const argv = process.argv
+  argv
+    .slice(argv.findIndex((arg) => arg.slice(0, 2) === '--'))
+    .forEach(arg => {
+      if (arg.slice(0, 2) === '--') {
+        const argNameValue = arg.split('=')
+        const argName = argNameValue[0].slice(2)
+        let argValue = argNameValue[1]
+        if (argValue[0] === "'" || argValue[0] === '"') {
+          argValue = argValue.slice(1)
+        }
+        if (argValue[argValue.length - 1] === "'" || argValue[argValue.length - 1] === '"') {
+          argValue = argValue.slice(0, -1)
+        }
+        args[argName] = argValue
+      }
+    })
+  return args
+}
